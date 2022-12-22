a820626dccbb6f450d0455eb8fed497e798ccd97
Денис Мучкаев <denmuch@MacBook-Pro-Denis.local>
2022-12-22 16:30:41 +0300

    Changed some values to HEROKU consts

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { handleValidationErrors, checkAuth } from "./utils/index.js"
 import { UserController, PostController } from "./controllers/index.js"
 
 
-mongoose.connect('mongodb+srv://admin:30061986Qaz@cluster0.slstb1r.mongodb.net/blog?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("DB ok"))
     .catch((err) => console.log("DB error", err))
 
@@ -47,7 +47,7 @@ app.post("/posts", checkAuth, postCreateValidation, handleValidationErrors, Post
 app.delete("/posts/:id", checkAuth, PostController.remove)
 app.patch("/posts/:id", checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if (err) {
         return console.log(err);
     }
